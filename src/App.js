f094fd41d84944e8eb16e1773925a961063eddb0
Achmad Rijalu <achmad.rijalu@gmail.com>
2022-01-16 11:42:13 +0700

    npm starting with react

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p className='text-green-300'>
+          This is my first time with react project
         </p>
         <a
           className="App-link"
